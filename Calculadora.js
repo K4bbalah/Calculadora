799ed5ada7094904fe8f31ea02828d1a9c8d0f12
Kabbalah <97350510+K4bbalah@users.noreply.github.com>
2022-09-02 13:48:45 -0300

    Update and rename sc.js to Calculadora.js

--- Calculadora.js
+++ Calculadora.js
@@ -7,7 +7,6 @@ var limite=0;
 var limiteP=0;
 var sound=false;
 var mais_menos=false;
-var tempo=1;
 var bloqueio3=false;
 
 
@@ -407,37 +406,6 @@ function igual(){
 
     
 }
-setInterval(function(){
-    switch (tempo){
-        case 1:
-            document.getElementById("estilo").style.backgroundImage= 'linear-gradient(#0101ff,#cbff7b )';
-            ++tempo;
-            break;
-        case 2:document.getElementById("estilo").style.backgroundImage= 'linear-gradient(#0000a4,#adff2f)';
-            ++tempo;
-            break;
-        case 3:document.getElementById("estilo").style.backgroundImage= 'linear-gradient(#000083,#92f200)';
-            ++tempo;
-            break;
-        case 4:document.getElementById("estilo").style.backgroundImage= 'linear-gradient(#151515,#fd51d1)';
-            ++tempo;
-            break;
-        case 5:document.getElementById("estilo").style.backgroundImage= 'linear-gradient(#111111,#fc0fc0)';
-            ++tempo;
-            break;
-        case 6:document.getElementById("estilo").style.backgroundImage= 'linear-gradient(#000000,#d3039e)';
-            ++tempo;
-            break;
-        case 7:document.getElementById("estilo").style.backgroundImage= 'linear-gradient(#b509ff,#87b6b8)';
-            ++tempo;
-            break;
-        case 8:document.getElementById("estilo").style.backgroundImage= 'linear-gradient(#9400d3,#5f9ea0)';
-            ++tempo;
-            break;
-        case 9:document.getElementById("estilo").style.backgroundImage= 'linear-gradient(#7600a9,#4c7e80)';
-            tempo=1;
-            break;
-    }
-},2000)
+
 
 
